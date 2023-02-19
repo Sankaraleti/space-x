a0bf250b154ab0442eb0e4b080f6570b4b6b591d
Sankaraleti <sankaraleti143@gmail.com>
2023-02-19 23:35:22 +0530

    Created and displayed launch component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Header from './Components/Header/Header'
+import Launch from './Components/Launch/Launch'
+import './style.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Launch
+        imageUrl="https://farm2.staticflickr.com/1633/25788014884_6a3f9ae183_o.jpg"
+        name="Falcon 9"
+        launchDate="2016-03-04T23:35:00.000Z"
+        description="Engine failure at 33 seconds and loss of vehicle.
+        Engine failure at 33 seconds and loss of vehicle.
+        Engine failure at 33 seconds and loss of vehicle.
+        Engine failure at 33 seconds and loss of vehicle.
+        Engine failure at 33 seconds and loss of vehicle."
+      />
+      <Launch
+        imageUrl="https://images2.imgbox.com/4b/bd/d8UxLh4q_o.png"
+        name="Trailblazer"
+        launchDate="2008-08-03T03:34:00.000Z"
+        description="Residual stage 1 thrust led to collision between stage 1 and stage 2"
+      />
     </div>
   );
 }
+
+
+// GitHub token
+// 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
 
 export default App;
---
+++ src/Components/Launch/Launch.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './style.css'
+const Launch = (props) => {
+    return (
+        <>
+            <div className="launch">
+                <div class="image">
+                    <img src={props.imageUrl} alt="Racket" />
+                </div>
+                <div className="info">
+                    <div className="details">
+                        <h1>{props.name}</h1>
+                        <h3>{props.launchDate}</h3>
+                    </div>
+                    <div className="description">
+                        <p>{props.description}</p>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Launch
